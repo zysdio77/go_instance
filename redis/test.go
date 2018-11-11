fd0baa6adaab8d0e9bed2c37d26e5c601fdd857d
张永生 <zhangyongsheng@zhangyongshengdeMacBook-Pro.local>
2018-11-11 23:38:32 +0800

    20181111

--- redis/test.go
+++ redis/test.go
@@ -1,25 +1,60 @@
 package main
 
 import (
-	"gopkg.in/redis.v4"
 	"fmt"
+	"gopkg.in/redis.v4"
 )
 
-func main()  {
-	//链接数据库
-	cli := redis.NewClient(&redis.Options{Addr:"192.168.2.237:6379",Password:""})
+func Sentinel() {
+	//链接sentibel
+	cli := redis.NewFailoverClient(&redis.FailoverOptions{
+		MasterName:    "mymaster",
+		SentinelAddrs: []string{"104.225.154.39:26379", "104.225.154.39:26380", "104.225.154.39:26381"},
+	})
+	defer cli.Close()
+
+	//插入数据
+	err := cli.Set("zhang", "zhangyongsheng", 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	//获取数据
-	value,err := cli.Get("xxx").Result()
+	value, err := cli.Get("zhang").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(value)
+}
+
+func usualy() {
+	//链接数据库
+	cli := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379", Password: ""})
+	defer cli.Close()
+
+	//插入数据
+	err := cli.Set("yong", "yong", 0).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//获取数据
+	value, err := cli.Get("yong").Result()
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(value)
 
-	//插入数据
-	err = cli.Set("zhang", "zhangyongsheng", 0).Err()
+	//删除数据
+	number, err := cli.Del("zhang").Result()
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Println(number)
 
+}
+
+func main() {
+
+	usualy()
 }
--- spider/spider_story/spider_story.go
+++ spider/spider_story/spider_story.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-	"gopkg.in/resty.v1"
-	"golang.org/x/text/transform"
 	"bytes"
-	"golang.org/x/text/encoding/simplifiedchinese"
-	"io"
-	"github.com/labstack/gommon/log"
-	"github.com/PuerkitoBio/goquery"
-	"os"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	_ "github.com/go-sql-driver/mysql"
+	"go.pkg.wesai.com/p/base_lib/log"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"gopkg.in/resty.v1"
+	"io"
+	"os"
 	"time"
 )
 
 type Introduce struct {
-	Author string
-	Name string
-	MenuPath	[]string
+	Author   string
+	Name     string
+	MenuPath []string
 	MenuName []string
-	Content string
+	Content  string
 }
 
-func FirstPage(url_addr string) ([]byte,error){
-	resp,err := resty.R().Get(url_addr)
+func FirstPage(url_addr string) ([]byte, error) {
+	resp, err := resty.R().Get(url_addr)
 	if err != nil {
-		return nil, err	
+		return nil, err
 	}
 	//readre := transform.NewReader(bytes.NewReader(resp.Body()),simplifiedchinese.GBK.NewDecoder()) //gbk转utf8
 	//fmt.Println(readre)
-	return resp.Body(),nil
+	return resp.Body(), nil
 }
 
-func GbkToUtf8(r []byte) io.Reader  {
+func GbkToUtf8(r []byte) io.Reader {
 	//r是传过来的resp.body()内容
-	readre := transform.NewReader(bytes.NewReader(r),simplifiedchinese.GBK.NewDecoder()) //gbk转utf8
+	readre := transform.NewReader(bytes.NewReader(r), simplifiedchinese.GBK.NewDecoder()) //gbk转utf8
 	return readre
 }
 
-func (introduce *Introduce)GetIntroduce(utf8_html io.Reader) (*Introduce,error) {
+func (introduce *Introduce) GetIntroduce(utf8_html io.Reader) (*Introduce, error) {
 	//goquery，从reader读出文档
-	doc,err := goquery.NewDocumentFromReader(utf8_html)
+	doc, err := goquery.NewDocumentFromReader(utf8_html)
 	if err != nil {
-		return introduce,err
+		return introduce, err
 	}
 	//过滤标签内容，查找出<div class="jieshao">下的<div class="rt">下的<h1>下的文本内容
 	introduce.Name = doc.Find("div.jieshao").Find("div.rt").Find("h1").Text()
@@ -54,111 +54,225 @@ func (introduce *Introduce)GetIntroduce(utf8_html io.Reader) (*Introduce,error) 
 	//循环遍历<div class="mulu">下的所有<li>标签
 	doc.Find("div.mulu").Find("li").Each(func(i int, s *goquery.Selection) {
 		//每条<li>下的<a>里href的属性 <a href = "属性">
-		ss,ok := s.Find("a").Attr("href")
+		ss, ok := s.Find("a").Attr("href")
 		//每一条<a>标签的文本内容 <a>文本内容</a>
-		neme :=s.Find("a").Text()
-		if ok{
-			introduce.MenuPath = append(introduce.MenuPath,ss)
+		neme := s.Find("a").Text()
+		if ok {
+			introduce.MenuPath = append(introduce.MenuPath, ss)
 		}
-		introduce.MenuName = append(introduce.MenuName,neme)
+		introduce.MenuName = append(introduce.MenuName, neme)
 	})
 	//fmt.Println(introduce.Menu[1])
-	return introduce,nil
+	return introduce, nil
 }
-func (introduce *Introduce)GetContent(host string) error {
-		resp,err := resty.R().Get(host)
-		r := GbkToUtf8(resp.Body())
-		if err != nil {
-			return err
-		}
-		doc,err := goquery.NewDocumentFromReader(r)
-		if err != nil{
-			return err
-		}
-		//s:= doc.Find("div.novel").Find("div.yd_text2").Text()
-		//找出<div class = "novel">下<div class="yd_text2">下的文本内容
-		introduce.Content= doc.Find("div.novel").Find("div.yd_text2").Text()
-		//fmt.Println("getcontent",introduce.Content)
+func (introduce *Introduce) GetContent(host string) error {
+	resp, err := resty.R().Get(host)
+	r := GbkToUtf8(resp.Body())
+	if err != nil {
+		return err
+	}
+	doc, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return err
+	}
+	//s:= doc.Find("div.novel").Find("div.yd_text2").Text()
+	//找出<div class = "novel">下<div class="yd_text2">下的文本内容
+	introduce.Content = doc.Find("div.novel").Find("div.yd_text2").Text()
+	//fmt.Println("getcontent",introduce.Content)
 	return nil
 }
-func WriteFile(words,filename string) error {
+func WriteFile(words, filename string) error {
 	//f,err :=os.Create(filename)
 	//打开文件，读写，如果没有则创建，追加的方式，权限644
-	f,err := os.OpenFile(filename,os.O_RDWR|os.O_CREATE|os.O_APPEND,0644)
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	defer f.Close()
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	//写入文字
-	_,err = f.WriteString(words)
-	if err != nil{
+	_, err = f.WriteString(words)
+	if err != nil {
 		return err
 	}
 
 	return nil
 
 }
-func ConnectDb() (*sql.DB,error) {
+func ConnectDb() (*sql.DB, error) {
 	//链接数据库
-	db,err := sql.Open("mysql","root:root@tcp(104.225.154.39:3306)/test?charset=utf8")
+	db, err := sql.Open("mysql", "root:root@tcp(104.225.154.39:3306)/test?charset=utf8")
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return db,nil
+	return db, nil
 }
-func (introduce *Introduce )WriteSql(db *sql.DB,k int)  {
-	//for k,_ := range introduce.MenuPath{
-		//fmt.Println(introduce.Name,introduce.Author,introduce.MenuPath[k],introduce.MenuName[k],introduce.Content)
-		//db.Exec("insert into story (name,author,menupath,menuname,content) values (?,?,?,?,?)",introduce.Name,introduce.Author,introduce.MenuPath[k],introduce.MenuName[k],introduce.Content)
-
+func (introduce *Introduce) WriteSql(db *sql.DB, k int,table_name string) {
+	sqlcmd := fmt.Sprintf("insert into %s (name,author,menupath,menuname,content) values (?,?,?,?,?)",table_name)
 	//准备执行sql
-	s,err :=db.Prepare("insert into story_01 (name,author,menupath,menuname,content) values (?,?,?,?,?)")
+	s, err := db.Prepare(sqlcmd)
 	if err != nil {
-		fmt.Println(err)
+		log.DLogger().Fatal(err)
 	}
+	defer s.Close()
 	//执行sql，把valuse传进去
-	r,err :=s.Exec(introduce.Name,introduce.Author,introduce.MenuPath[k],introduce.MenuName[k],introduce.Content)
-	if err != nil{
-		fmt.Println(err)
+	r, err := s.Exec(introduce.Name, introduce.Author, introduce.MenuPath[k], introduce.MenuName[k], introduce.Content)
+	if err != nil {
+		log.DLogger().Fatal(err)
 	}
 	fmt.Println(r.RowsAffected())
 	//}
 }
-
-func main()  {
-	var introduce Introduce
-	fmt.Println(time.Now())
-	url_addr := "https://www.88dush.com/xiaoshuo/0/801/"
-	r,err := FirstPage(url_addr)
+func drop_table(table_name string)  {
+	db,err := ConnectDb()
 	if err != nil {
-		log.Fatal(err)
+		log.DLogger().Fatal(err)
+	}
+	defer db.Close()
+	sqlcmd := fmt.Sprintf("DROP TABLE IF EXISTS `%s`",table_name)
+	stmt,err := db.Prepare(sqlcmd)
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	defer stmt.Close()
+	r, err :=stmt.Exec()
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	fmt.Println(r.RowsAffected())
+}
+
+func create_table(table_name string) {
+	db, err := ConnectDb()
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	defer db.Close()
+	sqlcmd := fmt.Sprintf("create table %s (id int auto_increment primary key,name varchar(100),author varchar(100),menupath varchar(100),menuname varchar(100),content longtext)", table_name)
+	stmt, err := db.Prepare(sqlcmd)
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	defer stmt.Close()
+	r, err := stmt.Exec()
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	fmt.Println(r.RowsAffected())
+}
+
+func alter_table(table_name string) {
+	db, err := ConnectDb()
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	defer db.Close()
+	sqlcmd := fmt.Sprintf("alter table %s AUTO_INCREMENT=%d", table_name, 1000000)
+	stmt, err := db.Prepare(sqlcmd)
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	defer stmt.Close()
+	r, err := stmt.Exec()
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	fmt.Println(r.RowsAffected())
+}
+
+func do_work(url_addr ,table_name string) {
+	start_time := time.Now().Unix()
+	var introduce Introduce
+	r, err := FirstPage(url_addr)
+	if err != nil {
+		log.DLogger().Fatal(err)
 	}
 	resp := GbkToUtf8(r)
 	//fmt.Println(string(b))
-	info ,err := introduce.GetIntroduce(resp)
+	info, err := introduce.GetIntroduce(resp)
 	if err != nil {
-		log.Fatal(err)
+		log.DLogger().Fatal(err)
 	}
+	fmt.Println(info)
+
 	//introduce.WriteFile("/Users/zhangyongsheng/Desktop/q.txt")
 	//introduce.WriteMysql()
 	//localpath := "/Users/zhangyongsheng/Desktop/"+introduce.Name+".txt"
 	//localpath := introduce.Name+".txt"
-	db,err :=ConnectDb()
+
+	db, err := ConnectDb()
 	defer db.Close()
-	for k,v := range introduce.MenuPath{
-		host := url_addr+v
-		//host := "https://www.88dush.com/xiaoshuo/0/801/247999.html"
+	for k, v := range introduce.MenuPath {
+		host := url_addr + v
 		err = introduce.GetContent(host)
 		if err != nil {
-			log.Fatal(err)
+			log.DLogger().Fatal(err)
 		}
 		if err != nil {
-			log.Fatal(err)
+			log.DLogger().Fatal(err)
 		}
 		//fmt.Println(info.Content)
-		info.WriteSql(db,k)
+		info.WriteSql(db, k,table_name)
 		//WriteFile(introduce.MenuName[k],localpath)
 		//WriteFile(s,localpath)
 	}
-	fmt.Println(time.Now())
+	end_time := time.Now().Unix()
+	used_second := end_time - start_time
+	display_minit := used_second / 60
+	display_second := used_second % 60
+	fmt.Printf("用时%d分钟：%d秒", display_minit, display_second)
+
+}
+func test(url_addr string) {
+	start_time := time.Now().Unix()
+	var introduce Introduce
+	r, err := FirstPage(url_addr)
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	resp := GbkToUtf8(r)
+	//fmt.Println(string(b))
+	info, err := introduce.GetIntroduce(resp)
+	if err != nil {
+		log.DLogger().Fatal(err)
+	}
+	fmt.Println(info)
+
+	//introduce.WriteFile("/Users/zhangyongsheng/Desktop/q.txt")
+	//introduce.WriteMysql()
+	//localpath := "/Users/zhangyongsheng/Desktop/"+introduce.Name+".txt"
+	//localpath := introduce.Name+".txt"
+
+	for k, v := range introduce.MenuPath {
+		host := url_addr + v
+		//host := "https://www.88dush.com/xiaoshuo/0/801/247999.html"
+		err = introduce.GetContent(host)
+		if err != nil {
+			log.DLogger().Fatal(err)
+		}
+		if err != nil {
+			log.DLogger().Fatal(err)
+		}
+		//fmt.Println(info.Content)
+			fmt.Println(introduce.MenuName[k],introduce.Content)
+		//WriteFile(introduce.MenuName[k],localpath)
+		//WriteFile(s,localpath)
+	}
+	end_time := time.Now().Unix()
+	used_second := end_time - start_time
+	display_minit := used_second / 60
+	display_second := used_second % 60
+	fmt.Printf("用时%d分钟：%d秒", display_minit, display_second)
+
+}
+
+func main() {
+	tablename := "story_zgj"
+	drop_table(tablename)
+	create_table(tablename)
+	alter_table(tablename)
+	//url_addr := "https://www.88dush.com/xiaoshuo/0/801/" //茅山后裔
+	//url_addr := "https://www.88dush.com/xiaoshuo/70/70239/"	//苗疆蛊事
+	url_addr := "https://www.88dush.com/xiaoshuo/65/65120/"	//捉蛊记
+	do_work(url_addr,tablename)
+	//test(url_addr)
 }
