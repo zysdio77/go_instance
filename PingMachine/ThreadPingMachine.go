5428b1500cb06b08f7e9e54549c24a260136ae23
张永生 <zhangyongsheng@zhangyongshengdeMacBook-Pro.local>
2020-09-11 14:07:04 +0800

    并发ping主机

--- PingMachine/ThreadPingMachine.go
+++ PingMachine/ThreadPingMachine.go
@@ -7,34 +7,34 @@ import (
 	"time"
 )
 
-func CheckMachine(shell string) int{
-	_,err := exec.Command("/bin/bash","-c",shell).Output()
+func CheckMachine(shell string) int {
+	_, err := exec.Command("/bin/bash", "-c", shell).Output()
 	if err != nil {
 		return 0
-	}else {
+	} else {
 		return 1
 	}
 }
-func threadip(i int,c chan int) {
-	ipaddr :="192.168.1."+strconv.Itoa(i)
-	shell := "ping -c 2 "+ ipaddr
+func threadip(i int, c chan int) {
+	ipaddr := "192.168.1." + strconv.Itoa(i)
+	shell := "ping -c 2 " + ipaddr
 	r := CheckMachine(shell)
 	c <- r
 	if r == 1 {
 		fmt.Println(i)
 	}
 }
-func main()  {
+func main() {
 	a := time.Now().Unix()
 	//fmt.Println(a)
-	c := make(chan int,10)
-	for i:=1;i<255;i++{
-		go threadip(i,c)
+	c := make(chan int, 10)
+	for i := 1; i < 255; i++ {
+		go threadip(i, c)
 	}
-	for i:=1;i<255;i++ {
-		<- c
+	for i := 1; i < 255; i++ {
+		<-c
 	}
 	b := time.Now().Unix()
 	//fmt.Println(b)
-	fmt.Println("共用时：",b-a,"秒")
+	fmt.Println("共用时：", b-a, "秒")
 }
